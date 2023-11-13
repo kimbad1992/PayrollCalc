3361c7be7ecf7d18a2cbc7d7fc3acb0be3ee2868
kimbad <kimbad@kakao.com>
2023-11-13 13:45:09 +0900

    DDL 내 Favicon Class정보 추가

--- DDL/DDL.sql
+++ DDL/DDL.sql
@@ -88,27 +88,13 @@ CREATE TABLE ADMIN_MENU (
                             level INT NOT NULL,
                             icon_class VARCHAR(100)
 );
-INSERT INTO public.admin_menu
-(page_seq, page_name, page_url, gnb_sort, gnb_name, role_id, parent_page_seq, "level")
-VALUES(1, '사원 관리', '/employee', 1, '사원', 1, NULL, 1);
-INSERT INTO public.admin_menu
-(page_seq, page_name, page_url, gnb_sort, gnb_name, role_id, parent_page_seq, "level")
-VALUES(2, '', '/employee/list', 1, '사원 조회', 1, 1, 2);
-INSERT INTO public.admin_menu
-(page_seq, page_name, page_url, gnb_sort, gnb_name, role_id, parent_page_seq, "level")
-VALUES(3, '', '/employee/register', 1, '사원 등록', 1, 1, 2);
-INSERT INTO public.admin_menu
-(page_seq, page_name, page_url, gnb_sort, gnb_name, role_id, parent_page_seq, "level")
-VALUES(4, '', '/employee/excel', 1, '사원 엑셀 일괄등록', 1, 1, 2);
-INSERT INTO public.admin_menu
-(page_seq, page_name, page_url, gnb_sort, gnb_name, role_id, parent_page_seq, level)
-VALUES (5, '급여 관리', '/payroll', 2, '급여', 1, null, 1);
-INSERT INTO public.admin_menu
-(page_seq, page_name, page_url, gnb_sort, gnb_name, role_id, parent_page_seq, level)
-VALUES (6, '시스템 설정', '/system', 2, '시스템', 1, null, 1);
-INSERT INTO public.admin_menu
-(page_seq, page_name, page_url, gnb_sort, gnb_name, role_id, parent_page_seq, level)
-VALUES (7, '', '/system/commonCode', 2, '공통코드 관리', 1, 6, 2);
+INSERT INTO public.admin_menu (page_seq, page_name, page_url, gnb_sort, gnb_name, role_id, parent_page_seq, level, icon_class) VALUES (1, '사원 관리', '/employee', 1, '사원', 1, null, 1, 'fa fa-address-book');
+INSERT INTO public.admin_menu (page_seq, page_name, page_url, gnb_sort, gnb_name, role_id, parent_page_seq, level, icon_class) VALUES (2, '', '/employee/list', 1, '사원 조회', 1, 1, 2, null);
+INSERT INTO public.admin_menu (page_seq, page_name, page_url, gnb_sort, gnb_name, role_id, parent_page_seq, level, icon_class) VALUES (3, '', '/employee/register', 1, '사원 등록', 1, 1, 2, null);
+INSERT INTO public.admin_menu (page_seq, page_name, page_url, gnb_sort, gnb_name, role_id, parent_page_seq, level, icon_class) VALUES (4, '', '/employee/excel', 1, '사원 엑셀 일괄등록', 1, 1, 2, null);
+INSERT INTO public.admin_menu (page_seq, page_name, page_url, gnb_sort, gnb_name, role_id, parent_page_seq, level, icon_class) VALUES (5, '급여 관리', '/payroll', 2, '급여', 1, null, 1, 'fa fa-credit-card');
+INSERT INTO public.admin_menu (page_seq, page_name, page_url, gnb_sort, gnb_name, role_id, parent_page_seq, level, icon_class) VALUES (6, '시스템 설정', '/system', 2, '시스템', 1, null, 1, 'fa fa-cog');
+INSERT INTO public.admin_menu (page_seq, page_name, page_url, gnb_sort, gnb_name, role_id, parent_page_seq, level, icon_class) VALUES (7, '', '/system/commonCode', 2, '공통코드 관리', 1, 6, 2, null);
 ------------------------------------------------------------------------------------------------------------------------
 CREATE TABLE GROUP_CODE (
                             GROUP_CODE_SEQ SERIAL PRIMARY KEY,
