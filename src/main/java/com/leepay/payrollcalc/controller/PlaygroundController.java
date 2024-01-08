dd331db5c90cf0c0a5ba31b4c120d492bb3cf980
kimbad <kimbad@kakao.com>
2024-01-08 10:24:11 +0900

    로컬 AI 응답 Papago 번역 API 추가

--- src/main/java/com/leepay/payrollcalc/controller/PlaygroundController.java
+++ src/main/java/com/leepay/payrollcalc/controller/PlaygroundController.java
@@ -17,7 +17,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.security.Principal;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/playground")
@@ -58,6 +66,23 @@ public class PlaygroundController {
         return "/playground/llm";
     }
 
+    @PostMapping("/translate.do")
+    @ResponseBody
+    public String translate(@RequestBody String message) {
+        String clientId = PropUtil.getProperty("system.naver-api-key.clientId");
+        String clientSecret = PropUtil.getProperty("system.naver-api-key.secretKey");
+
+        String apiURL = "https://openapi.naver.com/v1/papago/n2mt";
+        String text;
+        text = URLEncoder.encode(message, StandardCharsets.UTF_8);
+
+        Map<String, String> requestHeaders = new HashMap<>();
+        requestHeaders.put("X-Naver-Client-Id", clientId);
+        requestHeaders.put("X-Naver-Client-Secret", clientSecret);
+
+        return post(apiURL, requestHeaders, text);
+    }
+
     @PostMapping("/sendMail.do")
     @ResponseBody
     public String doSend(@RequestBody Mail mail) {
@@ -82,5 +107,61 @@ public class PlaygroundController {
     public StompMessage sendMessage(StompMessage message, Principal principal) {
         message.setUsername(principal.getName());
         return message;
+    }
+
+    private static String post(String apiUrl, Map<String, String> requestHeaders, String text){
+        HttpURLConnection con = connect(apiUrl);
+        String postParams = "source=en&target=ko&text=" + text; //원본언어: 한국어 (ko) -> 목적언어: 영어 (en)
+        try {
+            con.setRequestMethod("POST");
+            for(Map.Entry<String, String> header :requestHeaders.entrySet()) {
+                con.setRequestProperty(header.getKey(), header.getValue());
+            }
+
+            con.setDoOutput(true);
+            try (DataOutputStream wr = new DataOutputStream(con.getOutputStream())) {
+                wr.write(postParams.getBytes());
+                wr.flush();
+            }
+
+            int responseCode = con.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) { // 정상 응답
+                return readBody(con.getInputStream());
+            } else {  // 에러 응답
+                return readBody(con.getErrorStream());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("API 요청과 응답 실패", e);
+        } finally {
+            con.disconnect();
+        }
+    }
+
+    private static HttpURLConnection connect(String apiUrl){
+        try {
+            URL url = new URL(apiUrl);
+            return (HttpURLConnection)url.openConnection();
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("API URL이 잘못되었습니다. : " + apiUrl, e);
+        } catch (IOException e) {
+            throw new RuntimeException("연결이 실패했습니다. : " + apiUrl, e);
+        }
+    }
+
+    private static String readBody(InputStream body){
+        InputStreamReader streamReader = new InputStreamReader(body);
+
+        try (BufferedReader lineReader = new BufferedReader(streamReader)) {
+            StringBuilder responseBody = new StringBuilder();
+
+            String line;
+            while ((line = lineReader.readLine()) != null) {
+                responseBody.append(line);
+            }
+
+            return responseBody.toString();
+        } catch (IOException e) {
+            throw new RuntimeException("API 응답을 읽는데 실패했습니다.", e);
+        }
     }
 }
